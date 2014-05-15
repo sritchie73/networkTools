127866e9de4e7a7ac8ad53693310774e23d263ad
Scott Ritchie <sritchie73@gmail.com>
2014-05-15 10:37:07 +1000

    Added function for parsing CSV files NEO gives out

--- R/cytoscape_tools.R
+++ R/cytoscape_tools.R
@@ -59,3 +59,45 @@ writeEdgeTableFromAdj <- function(adj, filename, noEdge=c(0, NA), diag=FALSE,
 }
 
 `%nin%` <- function(...) !(`%in%`(...))
+
+#' Converts the output of \code{NEO} into something sane.
+#' 
+#' \code{NEO} outputs a massive CSV file that is not easily parseable, nor
+#' loadable into cytoscape. This function will parse this CSV file and write
+#' two tables to file: 
+#' 
+#' An edge table, filled with relevant columns, notably
+#' splitting out the \code{edge} column into Source and Target nodes, and 
+#' splitting the \code{Final.SNPs.LEO.NB.OCA} column into a vector of SNPs for 
+#' the target and source nodes respectively.
+#' 
+#' A second table, filled with SNP to Node relationships will also be output.
+#' 
+#' @param neofilename path to the neo csv file.
+#' @param outfile file prefix to output the .edge.table.csv and .snp.table.csv
+#'        files
+#' @export
+parseNEOcsv <- function(neofilename, outfile) {
+  neo.csv <- read.csv(neofilename, stringsAsFactors=FALSE)
+  snps <- strsplit(neo.csv$Final.SNPs.LEO.NB.OCA, ";")
+  df <- data.frame(
+    stringsAsFactors=FALSE,
+    Source = sapply(strsplit(neo.csv$edge, " -> "), `[`, 1),
+    Target = sapply(strsplit(neo.csv$edge, " -> "), `[`, 2),
+    CPA.Anchors = sapply(snps, function(x) gsub("fsnp.cpa=", "", x[1])),
+    OCA.Anchors = sapply(snps, function(x) gsub("fsnp.oca=", "", x[2])),
+    LEO.NB.OCA = neo.csv$LEO.NB.OCA,
+    LEO.NB.CPA = neo.csv$LEO.NB.CPA,
+    P.Model.AB = neo.csv$Model.P.value.AtoB,
+    P.weighted.LEO.NB.OCA = neo.csv$P.weighted.LEO.NB.OCA,
+    Z.WaldPath.AB = neo.csv$ZtestPathCoefficientAB,
+    P.WaldPath.AB = pnorm(neo.csv$ZtestPathCoefficientAB, lower.tail=FALSE),
+    PearsonCor = neo.csv$PearsonCor
+  )
+  
+  write.csv(df[!is.na(CPA.Anchors)], file=paste0(outfile, ".edge.table.csv"), 
+            quote=FALSE)
+  write.csv(df[is.na(CPA.Anchros), c("Source", "Target", "PearsonCor")],
+            file=paste0(outfile, ".snp.table.csv"), quote=FALSE)
+}
+
